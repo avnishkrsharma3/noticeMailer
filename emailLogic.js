1df20ab00ab8a07c0a1c0cd3e44c6ea88461721d
Avnish Kumar <avnishkrsharma3@gmail.com>
2021-02-20 19:13:20 +0530

    try again

---
+++ emailLogic.js
@@ -0,0 +1,22 @@
+"use strict";
+const nodemailer = require("nodemailer");
+// async..await is not allowed in global scope, must use a wrapper
+module.exports.sendEmail = async function (html) {
+  // settings generate 
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "avnishkrsharma2@gmail.com", // generated ethereal user
+      pass: "doemaaymebylcutb" // generated ethereal password
+    },
+  });
+  // send mail with defined transport object
+  await transporter.sendMail({
+    from: '"Avnish 🚀👻" <avnishkrsharma2@gmail.com>', // sender address
+    to: "uttamsingh8340@gmail.com, avnishkrsharma3@gmail.com", // list of receivers
+    subject: "Notice",
+    html: html, // html body
+  });
+}
